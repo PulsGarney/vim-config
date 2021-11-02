8f696a414666ca43d5eef1671a64ae218b72af1e
Puls Garney <pulsgarney@Puls-MacBook-Pro.local>
2021-11-03 00:16:54 +0800

    update *rc

--- reload.sh
+++ reload.sh
@@ -5,19 +5,4 @@ echo "System: $OSTYPE"
 cp ./rc/.vimrc ~/.vimrc
 cp ./rc/.screenrc ~/.screenrc
 
-cp ./rc/.shellrc ~/.shellrc
-. ~/.shellrc
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux
-    cp ./rc/.linuxrc ~/.linuxrc
-    . ~/.linuxrc
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # MacOS
-    cp ./rc/.macrc ~/.macrc
-    # . ~/.macrc
-
-fi
-
 echo "Reloaded!"
